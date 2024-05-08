bfef25241b063b2878edb4ce3f683c96024913be
Gilles Lucien <gpeltier2@gmail.com>
2024-05-08 16:25:17 +0200

    feat: Add login page, home page, footer, hero, and nav components

---
+++ argentbankapp/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hero } from "../../components/hero/Hero";
+import { Perks } from "../../components/perks/Perks";
+
+export function Home() {
+  return (
+    <main>
+      <Hero />
+      <Perks />
+    </main>
+  );
+}
---
+++ argentbankapp/src/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoginForm } from "../../components/loginForm/LoginForm";
+
+export function Login() {
+  return (
+    <main class="main bg-dark">
+      <LoginForm />
+    </main>
+  );
+}
